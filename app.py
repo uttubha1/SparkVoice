8139acb0081e5697dd73c15474acce7a12522617
utkarshtrip <utkarsh.tripathi@flairminds.com>
2024-04-19 17:59:06 +0530

    sentiment analysis done

--- app.py
+++ app.py
@@ -1,35 +1,14 @@
 from flask import Flask, request, jsonify
-from transformers import RobertaTokenizer, RobertaForSequenceClassification, Trainer, TrainingArguments, pipeline
-from datasets import load_dataset
-import os
+from transformers import AutoTokenizer, AutoModelForSequenceClassification, pipeline, RobertaTokenizer
 from werkzeug.utils import secure_filename
+import os
 
 app = Flask(__name__)
 
-# Fine-tune the RoBERTa model on the SST-2 dataset
-dataset = load_dataset("sst")
+# Load the sentiment analysis pipeline with the desired model
+model_name = "UtkarshFlairminds/sentimentAnalysis"
 tokenizer = RobertaTokenizer.from_pretrained("roberta-base")
-
-def tokenize_function(examples):
-    tokenized_inputs = tokenizer(examples["sentence"], padding="max_length", truncation=True)
-    # Rename the 'label' column to 'labels' for Trainer compatibility
-    tokenized_inputs["labels"] = [1 if label else 0 for label in examples["label"]]
-    return tokenized_inputs
-
-tokenized_datasets = dataset.map(tokenize_function, batched=True)
-
-model = RobertaForSequenceClassification.from_pretrained("roberta-base", num_labels=2)
-training_args = TrainingArguments("sst2", evaluation_strategy="epoch")
-trainer = Trainer(
-    model=model,
-    args=training_args,
-    train_dataset=tokenized_datasets["train"],
-    eval_dataset=tokenized_datasets["validation"],
-)
-trainer.train()
-
-
-# Load the fine-tuned model for sentiment analysis
+model = AutoModelForSequenceClassification.from_pretrained(model_name)
 sentiment_analysis = pipeline("sentiment-analysis", model=model, tokenizer=tokenizer)
 
 # Configure the upload folder
@@ -60,7 +39,7 @@ def analyze():
         if len(transcription) > max_length:
             transcription = transcription[:max_length]
 
-        # Use the fine-tuned model for sentiment analysis
+        # Use the sentiment analysis pipeline
         sentiment_result = sentiment_analysis(transcription)
 
         result = {
